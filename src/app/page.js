9617017a02736665a8ceaf1b2b210099c169d66e
abhisheksavaliya555 <abhisheksavaliya555@gmail.com>
2024-05-13 23:03:34 +0530

    add speedInsights

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 import Profile from './profile/page';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
     <>
+      <SpeedInsights />
       <Profile />
       <Analytics />
+
     </>
   );
 }
