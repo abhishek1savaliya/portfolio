369cfaa82b1d2ed9663aaee11de28dc7100ec7dd
abhisheksavaliya555 <abhisheksavaliya555@gmail.com>
2024-05-13 23:10:29 +0530

    add meta tage in main page

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,12 @@ import Profile from './profile/page';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
+
+export const metadata = {
+  title: 'Abhishek Savalia',
+  description: "Abhishek Savaliya's profile as a MERN Stack intern from Surat, Gujarat.",
+}
+
 export default function Home() {
   return (
     <>
