a992175477d678fd796e5d0644592f3290142eba
abhisheksavaliya555 <abhisheksavaliya555@gmail.com>
2024-05-19 22:04:25 +0530

    update meta tag in main page

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,48 @@ import Profile from './profile/page';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
-
 export const metadata = {
-  title: 'Abhishek Savalia',
+  title: 'Abhishek Savaliya',
   description: "Abhishek Savaliya's profile as a MERN Stack intern from Surat, Gujarat.",
+  name: 'ABHISHEK SAVALIYA',
+  location: 'Surat, Gujarat',
+  email: 'abhisheksavaliya555@gmail.com',
+  linkedin: 'https://linkedin.com/in/abhishek-savaliya-22547b200',
+  github: 'github.com/abhishek1savaliya',
+  skills: [
+    'MERN Stack (MongoDB, Express.js, React.js, Node.js)',
+    'Next.js framework',
+    'Version Control: Git',
+    'C, Java, Python (BASIC)',
+    'CSS, HTML, JavaScript',
+    'MongoDB, MySQL (Basic)'
+  ],
+  education: [
+    {
+      degree: 'Bachelor Of Engineering, Computer Engineering',
+      institution: 'Shri Swami Atmanand Saraswati Institute Of Technology - (GTU)',
+      year: '2020 - Pursuing 4th Year'
+    },
+    {
+      degree: 'Higher Secondary Certificate (HSC)',
+      institution: 'P.P. Savani Vidhya Bhavan(GSEB)',
+      year: '2020'
+    },
+    {
+      degree: 'Secondary School Certificate (SSC)',
+      institution: 'Ankur Vidhya Vihar (GSEB)',
+      year: '2018'
+    }
+  ],
+  projects: [
+    'Problem Solving: Word Counter (Frontend)',
+    'The Rick and Morty (Frontend)',
+    'Note App (Full Stack)'
+  ],
+  certifications: [
+    'Code Unnati: AI, IoT & ERP 2022-2023',
+    'Getting Started with Web Development - Record of Achievement'
+  ]
 }
 
 export default function Home() {
